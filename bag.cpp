5a70df9bdbffb09c960c38716fff3ed152f46958
VBarletta3 <62821802+VBarletta3@users.noreply.github.com>
2020-10-28 10:54:30 -0700

    Add files via upload

---
+++ bag.cpp
@@ -0,0 +1,30 @@
+#include "bag.h"
+#include<iostream>
+
+// YOU CAN INCLUDE OTHER HEADER FILES HERE IF NEEDED
+// DO NOT ADD THE FOLLOWING HEADERS:
+// 1. <vector>, 2. <queue>, 3. <deque>, 4. <stack>,
+// 5. <map>, 6. <algorithm>, 7. <set>, 8. <utility>
+// In summary do not use any built-in data structures
+// other than arrays. Also, do not use any built-in
+// algorithms related to sorting and searching
+
+// you are allowed to use the header files: <cstring>, <string>
+// you can use the string manipulation functions
+// provided by those header files if needed
+
+using namespace std;
+
+// implementation of word constructor; you may modify this if required
+Word::Word(std::string name){
+  word = name;
+  count = 1;
+}
+
+// Implement the print_words function
+void Bag::print_words() const {
+
+}
+
+// Implement any other memeber functions you may have
+// added to the class definitions
